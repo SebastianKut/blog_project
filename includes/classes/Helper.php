2ff92f5975ee51ca8036d498968eb3d1cdd20233
Sebastian Kut <sebastian_kut@yahoo.co.uk>
2020-11-11 11:04:24 +0100

    helper class completed

--- includes/classes/Helper.php
+++ includes/classes/Helper.php
@@ -9,19 +9,43 @@ class Helper{
             return false;
     };
 
-    public function isValidLength ($str, $min = 8, $max = 20) {
+    public function isValidLength($str, $min = 8, $max = 20) {
         if ( strlen($str) >= $min && strlen($str) <= $max ) 
             return true;
           else 
             return false;
     };
     
-    public function isEmpty ($postValues) {
+    public function isEmpty($postValues) {
         foreach($postValues as $value) {
             if ($value == '') 
                 return true;
             } 
             return false;
     };
+
+    public function isSecure($pw) {
+        if ( preg_match("~[a-z]+~", $pw) && preg_match("~[A-Z]+~", $pw) && preg_match("~[0-9]+~", $pw)) 
+            return true;
+          else
+            return false;
+    };
+
+    public function keepValues($val, $type, $attr) {
+        switch ($type){
+            case 'textbox':
+                echo "value = '$val'";
+                break;
+            case 'textarea':
+                echo $val;
+                break;
+            case 'select':
+                if ($val == $attr)
+                    echo 'selected';
+                break;
+            default:
+                echo '';
+            }
+    }
 
 }
