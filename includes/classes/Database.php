a0d69d0bb2d90faf15ab95dda419cf59c8d01ab5
Sebastian Kut <sebastian_kut@yahoo.co.uk>
2020-11-11 12:07:57 +0100

    database class done

--- includes/classes/Database.php
+++ includes/classes/Database.php
@@ -10,12 +10,30 @@ class Database{
 
     public function __construct(){
         
-        $pdo = new PDO("mysql:host=localhost;dbname=project", "project_admin", "Your Password");
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        $this->$pdo = new PDO("mysql:host=localhost;dbname=blog_project", "blog_admin", "test1234");
+        $this->$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
         
     }
 
     //Add queryDB() here
-    
+    public function queryDB($sql, $mode, $values = array()) {
+        // 1. prepare statement
+        $stmt = $this->$pdo->prepare($sql);
+        //2. bind values to the statement from multidimensional array $values 
+        //Svalues = [[:placeholder1, value1], [:placeholder2, value2],]
+        foreach($values as $valueToBind){
+            $stmt->bindValue($valueToBind[0], $valueToBind[1]);
+            };
+        //3. execute query    
+        $stmt->execute();    
+        //4. check if data has to be fetched after querry
+        if ( $mode != Database::SELECTSINGLE || $mode != Database::SELECTALL || $mode != Database::EXECUTE ) {
+            throw new Exception('Invalid mode');
+        } else if ( $mode == Database::SELECTSINGLE ) {
+           return $stmt->fetch(PDO::FETCH_ASSOC);
+        } else if ( $mode == Database::SELECTALL ) {
+           return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
 }
     
--- includes/classes/Helper.php
+++ includes/classes/Helper.php
@@ -45,7 +45,7 @@ class Helper{
                 break;
             default:
                 echo '';
-            }
+        }
     }
 
 }
