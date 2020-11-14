ee91283fe439bed6c700609d0b894376c7715683
Sebastian Kut <sebastian_kut@yahoo.co.uk>
2020-11-14 18:33:03 +0100

    read.php completed

--- includes/process/p-write.php
+++ includes/process/p-write.php
@@ -1,3 +1,36 @@
 <?php
 
-//start from page 182
+if( !isset($_SESSION['username']) || !isset($_SESSION['is_admin']) ) {
+
+    header('Location: admin.php');
+
+} else {
+
+    $h = new Helper();
+    $title = '';
+    $post = '';
+    $audience = ''; 
+    $msg = '';
+
+    if ( isset($_POST['submit']) ) {
+
+        $title = $_POST['title'];
+        $post = $_POST['post']; 
+        $audience = $_POST['audience'];
+
+        if ( $h->isEmpty([$title, $post, $audience]) ) {
+
+            $msg = 'All fields are required';
+
+        } else {
+
+            $admin = new Admin($_SESSION['$username']);
+
+            $admin->insertIntoPostDB($title, $post, $audience);
+
+            $msg = 'Message saved successfully';
+        }
+
+    }
+
+}
